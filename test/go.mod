6a7930491ef0f9ee0e32dac4c1218d6268601f95
nanato12 <admin@nanato12.info>
2020-11-21 17:52:11 +0900

    Add goimports failed case

--- test/go.mod
+++ test/go.mod
@@ -1,3 +1,5 @@
 module gocheck
 
 go 1.14
+
+require github.com/joho/godotenv v1.3.0
--- test/hello.go
+++ test/hello.go
@@ -1,7 +1,19 @@
 package hello
 
-import fmt "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+)
+
+// EnvLoad load .env
+func EnvLoad() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Error loading .env file")
+	}
+}
 
 func main() {
+	EnvLoad()
 	fmt.Printf("Hello, world!\n")
 }
